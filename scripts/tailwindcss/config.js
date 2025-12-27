cd2e5b18c76b9c01dc82c68d3e4c57305c5e788d
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-12-27 11:27:43 -0300

    Head da página com algumas configurações e SEO

---
+++ scripts/tailwindcss/config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#1e40af",  // azul principal
+        secondary: "#1e3a8a", // azul mais escuro hover
+      }
+    }
+  }
+}
