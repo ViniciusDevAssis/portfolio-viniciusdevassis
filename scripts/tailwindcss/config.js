04622e8f876a6cfb151cc4af327b9292c74ed16d
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2025-12-27 11:50:33 -0300

    Ajustes na página e definição do Body

--- scripts/tailwindcss/config.js
+++ scripts/tailwindcss/config.js
@@ -1,4 +1,5 @@
 tailwind.config = {
+  darkMode: 'media',
   theme: {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
@@ -7,6 +8,7 @@ tailwind.config = {
       colors: {
         primary: "#1e40af",  // azul principal
         secondary: "#1e3a8a", // azul mais escuro hover
+        dark_blue: "#151a29"  // azul mais escuro
       }
     }
   }
